eb8d8b0dacd704fcccf4c5381050f94b47d2f731
Parviz0 <100790765+Parviz0@users.noreply.github.com>
2023-01-14 18:02:22 +0500

    Add files via upload

---
+++ lesson/src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+import HeadScss from './sass/header.scss' 
+import MainScss from './sass/main.scss'
+import Header from './components/Header';
+import Main from './components/Main';
+
+
+function App() {
+  return (
+    <body>
+      <Header/>
+      <Main/>
+    </body>
+  );
+}
+
+
+
+export default App;
---
+++ lesson/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div className='header'>
+            <div className='logo'>
+                <h1>Ю.В</h1>
+                <h3>Сделано с душой</h3>
+            </div>
+            <div className="inHeader">
+                <p>Главня</p>
+                <p>Примеры работ</p>
+                <p>Контакты</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
